58f23f097099772850396e3f2c933766c3059c38
horsellcommon <crawfordj716@gmail.com>
2022-09-13 18:13:08 +0100

    Awful div clicker

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const percussion = document.getElementsByTagName("audio")
+const percussionbox = document.getElementsByClassName("percussion-box")
 console.log(percussion)
+console.log(percussionbox)
 document.addEventListener("keypress", () =>{
     if (event.which == "114"){
         percussion[0].play() //Boom
@@ -21,3 +23,34 @@ document.addEventListener("keypress", () =>{
         percussion[8].play() //Tom
     }
 })
+
+
+//I know below might come as a shock to the system but my brain stopped working
+
+const clicker = () => {
+        percussion[0].play()
+}
+const clicker1 = () => {
+    percussion[1].play()
+}
+const clicker2 = () =>{
+    percussion[2].play()
+}
+const clicker3 = () =>{
+    percussion[3].play()
+}
+const clicker4 = () =>{
+    percussion[4].play()
+}
+const clicker5 = () =>{
+    percussion[5].play()
+}
+const clicker6 = () =>{
+    percussion[6].play()
+}
+const clicker7 = () =>{
+    percussion[7].play()
+}
+const clicker8 = () =>{
+    percussion[8].play()
+}
